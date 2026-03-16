6b55acf78f1f73dd58f8f7e458fa7dbcd11ce517
farhansm01 <farhansadiq2021@gmail.com>
2026-03-16 20:59:54 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/Countries/Countries.jsx";
+
+function App() {
+  return (
+    <>
+      <h1>Get started</h1>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,9 @@
+const Countries = () => {
+  return (
+    <div>
+      <h2>In the countries</h2>
+    </div>
+  );
+};
+
+export default Countries;
